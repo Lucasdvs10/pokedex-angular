813fd950a9847cc05611c8581c851d4ec4112d9b
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-09-06 18:57:18 -0300

    Substitui o componente primario pelo componente de rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { CardsListComponentComponent } from './components/cards-list-component/cards-list-component.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CardsListComponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
